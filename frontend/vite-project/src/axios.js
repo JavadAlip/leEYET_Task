c09bd95e60580f2d2181bc2eb49bdb2dd2610810
JavadAlip <javadalipp911@gmail.com>
2025-02-07 10:23:38 +0530

    latest

--- frontend/vite-project/src/axios.js
+++ frontend/vite-project/src/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'https://leeyet-backend.onrender.com/api', //base URL
+    withCredentials: true
 });
 
 // Add the token to the headers before each request
