1d3f577cb2b35c7171cb0f2e9779b62b0d4e702f
JavadAlip <javadalipp911@gmail.com>
2025-02-07 11:43:14 +0530

    frontend updated

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Register from './pages/register1';
 import Login from './pages/login';
 import Dashboard from './pages/dashborad';
