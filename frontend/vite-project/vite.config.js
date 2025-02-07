920818ec56f8ff333d0b9793841b429763ce8b55
JavadAlip <javadalipp911@gmail.com>
2025-02-07 11:21:23 +0530

    updated

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,21 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Ensure this is set correctly for Render
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
-
